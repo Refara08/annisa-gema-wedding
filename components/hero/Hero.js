6c3c9b301d3eb5ac583a4a5e4af4377ae489123c
Gema Refantero <96099133+Refara08@users.noreply.github.com>
2022-09-26 20:35:04 +0700

    added hero section, not animated yet

---
+++ components/hero/Hero.js
@@ -0,0 +1,51 @@
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <section id="hero">
+      <div className="relative h-screen flex flex-col items-center justify-center gap-24">
+        {/* nama nisa gema dan ornamen */}
+        <div className="relative">
+          <h1 className="font-yaseva flex flex-col items-center leading-7 text-4xl">
+            <span>Annisa</span>
+            <span>&</span>
+            <span>Gema</span>
+          </h1>
+          <div className="absolute top-0 -translate-x-2 translate-y-3 scale-[300%]">
+            <Image
+              src={"/images/hero/flowers.png"}
+              alt="flowers ornaments"
+              width={"300px"}
+              height="220px"
+            />
+          </div>
+        </div>
+
+        {/* kalimat mengundang.. */}
+        <div className="w-[75%]">
+          <h2 className="font-sacramento text-3xl text-center">
+            mengundang Faris & pasangan ke pernikahan kami
+          </h2>
+        </div>
+
+        {/* tanggal pernikahan */}
+        <div>
+          <h2 className="font-prata md:text-lg">Sabtu, 15 Oktober 2022</h2>
+        </div>
+
+        {/* background */}
+        <div className="absolute bottom-36 md:-bottom-20  -z-10  w-full">
+          <Image
+            src={"/images/hero/bg-main.png"}
+            alt="main background of hero section"
+            width={"390px"}
+            height={"166px"}
+            layout="responsive"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
